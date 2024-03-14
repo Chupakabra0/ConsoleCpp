675ae2bd402935ac5b2f7faf1ed767e6e1be768f
Safiiulin Sasha <rootalfa4@gmail.com>
2024-03-14 23:38:26 +0200

    Remove fmt sources to use submodules

--- 3rdparty/fmt/support/cmake/FindSetEnv.cmake
+++
@@ -1,7 +0,0 @@
-# A CMake script to find SetEnv.cmd.
-
-find_program(WINSDK_SETENV NAMES SetEnv.cmd
-  PATHS "[HLOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\WindowsCurrentInstallFolder]/bin")
-if (WINSDK_SETENV AND PRINT_PATH)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E echo "${WINSDK_SETENV}")
-endif ()
